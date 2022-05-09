3310019e6fede9498cb9038b23f9f41cec70fd3d
Ethan Chandler <86173119+IzzTinkr@users.noreply.github.com>
2022-05-08 21:23:01 -0500

    image addition

--- nextjs-blog/pages/posts/first-post.js
+++ nextjs-blog/pages/posts/first-post.js
@@ -1,9 +1,25 @@
 import Link from "next/link";
+import Image from "next/Image";
+import Head from "next/head";
+import Script from "next/script";
 
+const DogFace = () => (
+  <Image
+    src="/public/images/DogFace.jpg"
+    height={144}
+    width={144}
+    alt="Portrait of a dog named Luna."
+  />
+);
 export default function FirstPost() {
   return (
     <>
+      <Head>
+        <title>First Post!!!!</title>
+      </Head>
+
       <h1>First Post</h1>
+
       <h2>
         <Link href="/">
           <a>Back to home</a>
